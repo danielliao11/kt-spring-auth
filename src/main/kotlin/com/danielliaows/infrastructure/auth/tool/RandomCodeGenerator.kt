a6bf643f7a02b54dbaf85ee94e7f0415c28fd2be
Yifan Liao <dan@Yifans-iMac.lan>
2020-02-16 16:45:09 +0800

    ADD: Add RandomCodeGenerator. There is three combinations to generate a random code. Only number, Only charactor And mixed.

--- src/main/kotlin/com/danielliaows/infrastructure/auth/tool/RandomCodeGenerator.kt
+++ src/main/kotlin/com/danielliaows/infrastructure/auth/tool/RandomCodeGenerator.kt
@@ -1,2 +1,24 @@
 package com.danielliaows.infrastructure.auth.tool
 
+import kotlin.random.Random
+
+class RandomCodeGenerator {
+    companion object {
+        private val numbers: CharArray = "0123456789".toCharArray()
+        private val characters: CharArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
+
+        fun random(length: Int, number: Boolean, character: Boolean) {
+            val source: CharArray = "".toCharArray()
+            if (number) {
+                source.plus(numbers)
+            }
+            if (character) {
+                source.plus(characters)
+            }
+            val result = StringBuilder()
+            for (i in 0..length) {
+                result.append(source[Random.nextInt()])
+            }
+        }
+    }
+}
